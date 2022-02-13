efec5521e7a916d099c47772eec6b5741a494351
Ertuğrul Gürkan <ertugrulgurkan16@gmail.com>
2022-02-14 00:41:36 +0300

    multiple microservice instance ex.

--- PhotoAppApiUsers/src/main/java/com/ertugrul/photoapp/api/users/controllers/UsersController.java
+++ PhotoAppApiUsers/src/main/java/com/ertugrul/photoapp/api/users/controllers/UsersController.java
@@ -1,6 +1,7 @@
 package com.ertugrul.photoapp.api.users.controllers;
 
 
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,8 +10,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("users")
 public class UsersController {
 
+    private final Environment environment;
+
+    public UsersController(Environment environment) {
+        this.environment = environment;
+    }
+
     @GetMapping("/status/check")
     public String status(){
-        return "Working OK.";
+        return "Working on port " + environment.getProperty("local.server.port");
     }
 }
